b718c1096a6c43b745f776a85a8f1c5036e92bdd
Brian Blais <bblais@gmail.com>
2023-09-08 11:34:44 -0400

    first day

---
+++ Sprint #1 - Programming and Games/2023-09-08 First Notebook.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("hello world")
+
+
+# In[2]:
+
+
+pwd
+
+
+# # Markdown
+# 
+# https://www.markdownguide.org/basic-syntax/
+# 
+# ## subheading
+# 
+# Some text here.  Maybe some **bold** or *italic*.
+# 
+# - how
+# - about
+# - a
+# - bulleted list?
+# 
+# Even equations like
+# 
+# $$
+# E=mc^2 \times \sqrt{\frac{1}{1-v^2/c^2}}
+# $$
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
