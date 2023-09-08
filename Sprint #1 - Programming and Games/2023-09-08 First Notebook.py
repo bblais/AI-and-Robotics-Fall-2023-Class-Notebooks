4aa6ef841cfdbc389396b74ebae24d8e930c1f3b
Brian Blais <bblais@gmail.com>
2023-09-08 12:02:05 -0400

    first again

--- Sprint #1 - Programming and Games/2023-09-08 First Notebook.py
+++ Sprint #1 - Programming and Games/2023-09-08 First Notebook.py
@@ -32,22 +32,122 @@ pwd
 # E=mc^2 \times \sqrt{\frac{1}{1-v^2/c^2}}
 # $$
 
-# In[ ]:
+# In[3]:
 
 
+3746+34838*2323
 
 
-
-# In[ ]:
-
+# In[4]:
 
 
+a=5
+b=6
+a*b
 
 
-# In[ ]:
+# In[5]:
 
 
+bob=7847
+sally=3434
+bob-sally
 
+
+# To do exponents, use ** not ^
+
+# In[6]:
+
+
+5**3
+
+
+# In[8]:
+
+
+5^3  # gives a weird answer and is not an exponent
+
+
+# In[9]:
+
+
+cos(45)
+
+
+# In[10]:
+
+
+print(c))
+
+
+# In[11]:
+
+
+print(c)
+
+
+# In[12]:
+
+
+a+b=a*b=a/b
+
+
+# In[13]:
+
+
+a
+
+
+# $$
+# 6a-4
+# $$
+
+# In[14]:
+
+
+6a-4
+
+
+# In[15]:
+
+
+6*a-4
+
+
+# In[16]:
+
+
+a
+
+
+# In[17]:
+
+
+a=a+1
+
+
+# In[18]:
+
+
+a
+
+
+# In[19]:
+
+
+a==6
+
+
+# In[20]:
+
+
+a+b==a*b
+
+
+# In[21]:
+
+
+a,b
 
 
 # In[ ]:
