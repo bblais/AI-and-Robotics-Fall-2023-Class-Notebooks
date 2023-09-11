f2c51f650f6c7a4bbd0dade1d8c4b11c8ace4ec9
Brian Blais <bblais@gmail.com>
2023-09-11 12:24:45 -0400

    first day

--- Sprint #1 - Programming and Games/2023-09-08 - First Turtle.py
+++ Sprint #1 - Programming and Games/2023-09-08 - First Turtle.py
@@ -1,29 +1,73 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[4]:
 
 
 get_ipython().run_line_magic('matplotlib', 'inline')
 from mplturtle import *
 
 
-# In[2]:
+# In[6]:
 
 
-reset()
+reset(figsize=(3,3))
 forward(50)
 right(90)
 forward(50)
 
 
-# In[3]:
+# In[8]:
 
 
-reset()
+reset(figsize=(3,3))
 forward(50)
 right(30)
 forward(50)
+
+
+# In[9]:
+
+
+def house(length):
+    pass
+
+
+# In[ ]:
+
+
+reset()
+house(30)
+forward(30)
+house(30)
+
+
+# In[ ]:
+
+
+bob=30
+reset()
+house(bob)
+forward(bob)
+house(bob)
+
+
+# In[ ]:
+
+
+
+
+
+# In[12]:
+
+
+reset(figsize=(3,3))
+forward(60)
+penup()
+forward(60)
+pendown()
+right(90)
+forward(60)
 
 
 # In[ ]:
--- Sprint #1 - Programming and Games/2023-09-11 - Intro Programming.py
+++ Sprint #1 - Programming and Games/2023-09-11 - Intro Programming.py
@@ -1,14 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
 
+# ## Repetition
+
+# In[1]:
 
 
 a=5
 print("hello",a)
 
 
+# In[3]:
+
+
 b=100
 
 
+# In[8]:
+
+
 b
+
+
+# In[5]:
 
 
 print("here")
@@ -20,8 +34,14 @@ for i in range(5):
 print("done")
 
 
+# In[7]:
+
+
 print("hello")
 Print("hello")
+
+
+# In[10]:
 
 
 print("here")
@@ -33,12 +53,18 @@ for i in range(15):
 print("done")
 
 
+# In[11]:
+
+
 print("here")
 for i in range(5):
     print("hello",i)
     print("there")
 
 print("done")
+
+
+# In[12]:
 
 
 print("here")
@@ -49,7 +75,9 @@ for bob in range(5):
 print("done")
 
 
+# ## Branching (if-then)
 
+# In[14]:
 
 
 print("here")
@@ -62,14 +90,22 @@ for bob in range(5):
 print("done")
 
 
+# In[ ]:
 
 
 
 
+
+# ## Defining functions
+
+# In[15]:
 
 
 print("hello")
 print("there")
+
+
+# In[17]:
 
 
 print("here")
@@ -81,9 +117,15 @@ for i in range(5):
 print("done")
 
 
+# In[18]:
+
+
 def hellothere():
     print("hello")
     print("there")    
+
+
+# In[19]:
 
 
 print("here")
@@ -93,6 +135,9 @@ for i in range(5):
 print("done")
 
 
+# In[21]:
+
+
 print("here")
 for i in range(5):
     Hellothere()
@@ -100,11 +145,18 @@ for i in range(5):
 print("done")
 
 
+# In[20]:
+
+
 print("here")
 for i in range(5):
     helloagain()
 
 print("done")
+
+
+# In[ ]:
+
 
 
 
