22c45df29b86d935d8fb24c2c559df9a76d5c8dd
Brian Blais <bblais@gmail.com>
2023-09-11 11:06:57 -0400

    turtle

---
+++ Sprint #1 - Programming and Games/2023-09-08 - First Turtle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+from mplturtle import *
+
+
+# In[2]:
+
+
+reset()
+forward(50)
+right(90)
+forward(50)
+
+
+# In[3]:
+
+
+reset()
+forward(50)
+right(30)
+forward(50)
+
+
+# In[ ]:
+
+
+
+
