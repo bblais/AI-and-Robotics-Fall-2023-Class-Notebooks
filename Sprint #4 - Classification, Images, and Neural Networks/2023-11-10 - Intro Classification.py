c4f45246f4610dc4d3fc37ae3db2b44ef90a65fe
Brian Blais <bblais@gmail.com>
2023-11-10 10:51:41 -0500

    intro classification

---
+++ Sprint #4 - Classification, Images, and Neural Networks/2023-11-10 - Intro Classification.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+from pylab import *
+
+
+# In[3]:
+
+
+from classy import *
+
+
+# In[4]:
+
+
+data=load_excel('data/iris.xls')
+
+
+# In[ ]:
+
+
+
+
